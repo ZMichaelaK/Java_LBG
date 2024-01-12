3c7d5f6b864088dbf4bbca244255853bb676ec65
ZMichaelaK <korn_miku@hotmail.com>
2024-01-12 12:13:46 +0000

    being silly

---
+++ src/exercise01/Exercise01.java
@@ -0,0 +1,34 @@
+package exercise01;
+
+public class Exercise01 {
+	public static void main(String[] args) {
+		byte blep = 5;
+		short sh = 4000;
+		int xs = 9999;
+		long k = 3472374L;
+		
+		float m = 23.34F;
+		double kk = 23.59;
+		
+		boolean y = true;
+		boolean x = false;
+		char a = 'M';
+		String name = "ichaela";
+		System.out.println(blep);
+		System.out.println(sh);
+		System.out.println(xs);
+		System.out.println(k);
+		System.out.println(m);
+		System.out.println(kk);
+		System.out.println(y);
+		System.out.println(x);
+		System.out.println("My name is " + a + name);
+		System.out.println(kk + blep + sh + xs);
+//		int z = null;
+//		System.out.println(z);
+		char z = 'a';
+		char tt = 'b';
+//		byte llll = 3.4
+		System.out.println(z + tt);
+	}
+}
